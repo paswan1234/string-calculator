28a4f2606d9673639eee9ce5c8ed8479df870b0a
paswan1234 <paswan1234@gmail>
2024-11-22 16:49:26 +0530

    Fix for NaN in return

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -4,9 +4,14 @@ export function stringCalculator(input: string): number {
     //check for empty string and retun zero
     if (input === "") return 0;
 
-    //split and add input numbers
-    return input
-        .split("/,|\n/") //RegEx to accept commas and newlines
-        .map(Number)
-        .reduce((sum, num) => sum + num, 0);
+    const delimiters = /,|\n/; // Match commas or newlines
+    const numbers = input.split(delimiters).map((str) => Number(str.trim()));
+
+    //validate the number array to ensure all elements are numeric
+    if (numbers.some(isNaN)) {
+        throw new Error("Invalid numberin input");
+    }
+
+
+    return numbers.reduce((sum, num) => sum + num, 0);
 }
